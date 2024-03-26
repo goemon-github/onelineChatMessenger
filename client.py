9374770735e9b3890ce289396445fca3635c4c1a
goemon-github <booya.8108@gmail.com>
2024-03-26 23:20:18 +0900

    refactoring to class-based

--- client.py
+++ client.py
@@ -1,84 +1,86 @@
 import socket
 
 
-
-def input_username():
-    while True:
-        username = input('username: ')
-        username_encoded = username.encode('utf-8')
-
-        if len(username_encoded) <  255:
-            break
-        else:
-            raise ValueError('over username 255')
-
-    return username_encoded
+class Client:
+    def __init__(self):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.server = self.input_server()
+        self.username = self.input_username()
+        self.message = self.input_message()
 
 
-def input_message():
-    while True:
-        message = input('message: ')
-        message_encoded = message.encode('utf-8')
+    def run(self):
+        print('connecting to {}'.format(self.server['address'], self.server['port']))
+        while True:
+            full_message = self.create_message(self.username, self.message)
 
-        if len(message_encoded) < 4096:
-            break
-        else:
-            print('error: message oversize')
-    return message_encoded
+            print('send Massge {} '.format(full_message))
 
+            sent = self.sock.sendto(full_message,  self.server)
+            print('sent to byte {}'.format(sent))
 
-def create_message(username, message):
-    header = len(username).to_bytes(1, byteorder='big')
-    full_message = header + username + message
-    return full_message
+            # レシーバーの実装
+            responce_data, responce_server = self.sock.recvfrom(4096)
 
+            if responce_data:
+                responce_username, responce_name_byte_length = self.get_username(responce_data)
+                responce_message = self.get_message(responce_data, responce_name_byte_length)
 
-def get_username(data):
-    name_byte_length = int.from_bytes(data[:1], 'big')
-    data = data.decode('utf-8')
-    username = data[1:1 + name_byte_length]
-
-    return username ,name_byte_length
-
-def get_message(data, name_byte_length):
-    data = data.decode('utf-8')
-    message = data[1+name_byte_length:]
-    return message 
+                print('server: {}, username: {}\n, message: {}'.format( responce_server, responce_username, responce_message))
 
 
+    def input_server(self):
+        server_address = '127.0.0.1'
+        server_port = 9001
+        server = {'address': server_address, 'port': server_port}
+        return server
 
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    def input_username(self):
+        while True:
+            username = input('username: ')
+            username_encoded = username.encode('utf-8')
+
+            if len(username_encoded) <  255:
+                break
+            else:
+                raise ValueError('over username 255')
+
+        return username_encoded
 
 
-server_address = '127.0.0.1'
-server_port = 9001
-server = (server_address, server_port)
+    def input_message(self):
+        while True:
+            message = input('message: ')
+            message_encoded = message.encode('utf-8')
 
-print('connecting to {}'.format(server_address, server_port))
-
-
-username = input_username()
-
-while True:
-    message = input_message()
-    full_message = create_message(username, message)
-
-    print('send Massge {} '.format(full_message))
-    #print('send Massge {} '.format(full_message.decode()))
-
-    sent = sock.sendto(full_message,  server)
-    print('sent to byte {}'.format(sent))
-
-    # レシーバーの実装
-    data, server = sock.recvfrom(4096)
-
-    if data:
-        responce_username, responce_name_byte_length = get_username(data)
-        responce_message = get_message(data, responce_name_byte_length)
-
-        print('server: {}, username: {}\n, message: {}'.format( server, responce_username, responce_message))
+            if len(message_encoded) < 4096:
+                break
+            else:
+                print('error: message oversize')
+        return message_encoded
 
 
-   
-   
+    def create_message(self, username, message):
+        header = len(username).to_bytes(1, byteorder='big')
+        full_message = header + username + message
+        return full_message
+
+
+    def get_username(self, data):
+        name_byte_length = int.from_bytes(data[:1], 'big')
+        data = data.decode('utf-8')
+        username = data[1:1 + name_byte_length]
+
+        return username ,name_byte_length
+
+    def get_message(self, data, name_byte_length):
+        data = data.decode('utf-8')
+        message = data[1+name_byte_length:]
+        return message 
+
+
+
+if __name__ == '__main__':
+    client = Client()
+    client.run()
--- server.py
+++ server.py
@@ -8,82 +8,91 @@ class ClientInfo:
         self.last_activity_time = time.time()
 
 
-clients = {} 
+class Server:
+    def __init__(self, host='0.0.0.0', port=9001, timeout=30) :
+        self.host = host  
+        self.port = port
+        self.sock = self.create_socket()
+        self.timeout = timeout
+        self.clients = {}
 
+    def create_socket(self):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.bind(self.host, self.port)
+        print('Listen.............')
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        return sock
 
-server_address = '0.0.0.0'
-server_port = 9001
-server = (server_address, server_port)
+    def run(self):
+        while True:
+            data, address = self.sock.recvfrom(4096)
 
-sock.bind(server)
+            username, name_byte_length = self.get_username(data)
 
+            self.update_client_activity(username, address)
+            message = self.get_message(data, name_byte_length)
 
-timeout = 60 
-#def check_nonactive_client():
+            print('username: {}, message: {}'.format(username.encode(), message))
 
-def get_username(data):
-    name_byte_length = int.from_bytes(data[:1], 'big')
-    username = data[1:1 + name_byte_length].decode('utf-8')
+            if data:
+                # 現在時刻を取得 
+                current_time = time.time()
 
-    return username, name_byte_length
+                print('data: {}, address: {}'.format( data.decode(), address))
+                inactive_clients = [username for username, info in self.clients.items() if current_time - info.last_activity_time > self.timeout]
+                self.delete_inactive_client(inactive_clients)
 
-def get_message(data, name_byte_length):
-    message = data[1+name_byte_length:].decode('utf-8')
-    return message 
+                for client_username, client_info in self.clients.items():
+                    if client_username != username and current_time - client_info.last_activity_time < self.timeout:
 
+                        full_message = self.create_full_message(username, message) 
 
-def update_client_activity(username, address):
-    if username in clients:
-        clients[username].last_activity_time = time.time()
-    else:
-        clients[username] = ClientInfo(address)
-
-def delete_inactive_client(inactive_clients):
-    for inactive_client in inactive_clients:
-        del clients[inactive_client]
-        print('delete {}'.format(inactive_client))
-
-
-def create_full_message(username, message):
-    header = len(username).to_bytes(1, byteorder='big')
-    username = username.encode()
-    message = message.encode()
-    full_message = header + username + message
-    print('header: {}, username: {}, message: {}'.format(header, username, message))
-    return full_message
+                        self.sock.sendto(full_message, client_info.address)
+                        print('relay {}'.format(client_info.address))
 
 
 
+    def update_timeout(self, timeout):
+        self.timeout = timeout
 
-while True:
-    print('Lissten.........')
-    data, address = sock.recvfrom(4096)
+    
+    #def  process_recived_message(data):
 
+        
+    def get_username(self, data):
+        name_byte_length = int.from_bytes(data[:1], 'big')
+        username = data[1:1 + name_byte_length].decode('utf-8')
 
-    username, name_byte_length = get_username(data)
+        return username, name_byte_length
 
-    update_client_activity(username, address)
-    message = get_message(data, name_byte_length)
-
-    print('username: {}, message: {}'.format(username.encode(), message))
-
-
-    if data:
-        # 現在時刻を取得 
-        current_time = time.time()
-
-        print('data: {}, address: {}'.format( data.decode(), address))
-        inactive_clients = [username for username, info in clients.items() if current_time - info.last_activity_time > timeout]
-        delete_inactive_client(inactive_clients)
-
-        for client_username, client_info in clients.items():
-            if client_username != username and current_time - client_info.last_activity_time < timeout:
-
-                full_message = create_full_message(username, message) 
-
-                sock.sendto(full_message, client_info.address)
-                print('relay {}'.format(client_info.address))
+    def get_message(self, data, name_byte_length):
+        message = data[1+name_byte_length:].decode('utf-8')
+        return message 
 
 
+    def update_client_activity(self, username, address):
+        if username in self.clients:
+            self.clients[username].last_activity_time = time.time()
+        else:
+            self.clients[username] = ClientInfo(address)
+
+    def delete_inactive_client(self, inactive_clients):
+        for inactive_client in inactive_clients:
+            del self.clients[inactive_client]
+            print('delete {}'.format(inactive_client))
+
+
+    def create_full_message(self, username, message):
+        header = len(username).to_bytes(1, byteorder='big')
+        username = username.encode()
+        message = message.encode()
+        full_message = header + username + message
+        print('header: {}, username: {}, message: {}'.format(header, username, message))
+        return full_message
+
+        
+
+
+if __name__ == '__main__':
+    server = Server()
+    server.run()
